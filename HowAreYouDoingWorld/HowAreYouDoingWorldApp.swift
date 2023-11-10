a334b1278a9b74aadde9d91783316d29527071b8
Mark Cornelisse <m.p.cornelisse@gmail.com>
2023-11-10 16:06:42 +0100

    Initial Commit

---
+++ HowAreYouDoingWorld/HowAreYouDoingWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowAreYouDoingWorldApp.swift
+//  HowAreYouDoingWorld
+//
+//  Created by Mark Cornelisse on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HowAreYouDoingWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
